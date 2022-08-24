e53c9e6e2e759afa8888e3a0a8fe7aae6fb8b2c8
Agustín Comolli <agustincomolli@gmail.com>
2022-08-23 22:21:32 -0300

    Formateando codigo.

--- freeCodeCamp/Mi primer proyecto/choose_your_adventure.py
+++ freeCodeCamp/Mi primer proyecto/choose_your_adventure.py
@@ -6,7 +6,7 @@ health = 10
 
 if age >= 18:
     print("¡Bien! Eres lo suficientemente mayor para jugar.")
-    wants_to_play = input("Quieres jugar? [si] ").lower() # -> Minúsculas.
+    wants_to_play = input("Quieres jugar? [si] ").lower()  # -> Minúsculas.
     if wants_to_play == "si":
         print("¡Juguemos!")
 
@@ -14,12 +14,12 @@ if age >= 18:
 
         left_or_right = input("Primera opción... Izquierda o Derecha " +
                               "(👈 izquierda/derecha 👉)? ").lower()
-        
+
         if left_or_right == "izquierda":
-            answer = input("Bien, sigues el camino y llegas a un lago..." + 
-                             "¿Lo cruzas a nado o das la vuelta alrededor " +
-                             "(🏊‍♂️ nadar/rodear 🚶‍♂️)? ").lower()
-            
+            answer = input("Bien, sigues el camino y llegas a un lago..." +
+                           "¿Lo cruzas a nado o das la vuelta alrededor " +
+                           "(🏊‍♂️ nadar/rodear 🚶‍♂️)? ").lower()
+
             if answer == "nadar":
                 print("Conseguiste cruzar, pero te mordió un pez 🐟 y " +
                       "perdiste 5 de vida.")
@@ -31,31 +31,31 @@ if age >= 18:
                       "maleza ha salido un oso, te ha atrapado y ahora " +
                       "eres su comida. ¡Perdiste! ☠️")
                 exit
-            
+
             answer = input("Notas que hay una casa y un río. ¿Dónde " +
                            "quieres ir? (🏡 casa/río 🌅) ")
             if answer == "casa":
-                print("Vas hacia la casa y su dueño piensa que vas a " + 
-                      "robarle, busca su escopeta y te recibe a balazos💥. " + 
+                print("Vas hacia la casa y su dueño piensa que vas a " +
+                      "robarle, busca su escopeta y te recibe a balazos💥. " +
                       "Uno de los perdigones da en tu brazo y pierdes 5 " +
                       "de vida. 🤕")
                 health -= 5
                 if health == 0:
                     print("Mueres a causa de tus heridas. ¡Perdiste! ☠️")
                 else:
-                    print("Alguien escucha el disparo de la escopeta y " + 
-                          "llama a la policía 🚔. Después de un rato dan " + 
+                    print("Alguien escucha el disparo de la escopeta y " +
+                          "llama a la policía 🚔. Después de un rato dan " +
                           "contigo y eres llevado a un hospital 🏥.")
                     print("Has logrado sobrevivir. ¡Ganaste el juego!")
             else:
                 print("Vas hacia el río cuando de repente sale un oso " +
-                      "pardo que te estaba siguiendo. No puedes escapar, " + 
-                      "caes en sus garras y pasas a ser su cena. " + 
+                      "pardo que te estaba siguiendo. No puedes escapar, " +
+                      "caes en sus garras y pasas a ser su cena. " +
                       "¡Perdiste! ☠️")
         else:
             print("Caminas unos pasos y te resbalas con una cáscara de " +
-                  "banana 🍌, pierdes la estabilidad y caes al suelo " + 
-                  "golpeando tu cabeza contra una piedra y mueres... " + 
+                  "banana 🍌, pierdes la estabilidad y caes al suelo " +
+                  "golpeando tu cabeza contra una piedra y mueres... " +
                   "¡Perdiste! ☠️")
     else:
         print("Hasta pronto...")
