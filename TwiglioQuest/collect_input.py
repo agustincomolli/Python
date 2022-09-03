295b3da1a02df7230224c750e78d6371bf7d7b87
Agustín Comolli <agustincomolli@yahoo.com.ar>
2022-09-03 15:47:43 -0300

    Agregados archivos de TwiglioQuest

---
+++ TwiglioQuest/collect_input.py
@@ -0,0 +1,6 @@
+import sys
+
+print(f"El nombre del archivo de Python es: '{sys.argv[0]}'")
+print(f"El primer argumento pasado a mi script fue: '{sys.argv[1]}'")
+print(f"El segundo argumento pasado a mi script fue: '{sys.argv[2]}'")
+print(f"El tercer argumento pasado a mi script fue: '{sys.argv[3]}'")
