d7a9403c662556ec3c43d57fbc2463ddf020d00c
Agustín Comolli <agustincomolli@yahoo.com.ar>
2022-10-07 15:43:41 -0300

    Ejercicios de Codedex

---
+++ Codedex/03-iniciales.py
@@ -0,0 +1,9 @@
+# Lo de abajo son mis iniciales: Agustín Alfredo Comolli
+
+print("  A      A     CCC ")
+print(" A A    A A   C   C")
+print("A   A  A   A  C")
+print("AAAAA  AAAAA  C")
+print("A   A  A   A  C")
+print("A   A  A   A  C   C")
+print("A   A  A   A   CCC ")
