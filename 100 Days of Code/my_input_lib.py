4183f7f0adac66062ce4d1eb140467be1fb000c1
Agustín Comolli <agustincomolli@yahoo.com.ar>
2022-12-17 12:12:13 -0300

    Agregados módulos my_math_lib y my_input_lib

---
+++ 100 Days of Code/my_input_lib.py
@@ -0,0 +1,118 @@
+import re
+
+
+def input_email(message):
+    """
+    Pide al usuario que ingrese un email y valida la entrada.
+
+    Args:
+    message (str): mensaje a mostrar al usuario al pedir el email.
+
+    Returns:
+    str: email válido ingresado por el usuario.
+    """
+    # Expresión regular para validar un email
+    pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+
+    # Pedimos al usuario que ingrese un email
+    email = input(message)
+
+    # Validamos la entrada del usuario
+    while not re.match(pattern, email):
+        print("\nEmail inválido, ingrese un email válido")
+        email = input(message)
+
+    return email
+
+
+def input_date(message):
+    """
+    Pide al usuario que ingrese una fecha en formato dd/mm/yyyy y valida la entrada.
+
+    Args:
+    message (str): mensaje a mostrar al usuario al pedir la fecha.
+
+    Returns:
+    str: fecha válida en formato dd/mm/yyyy ingresada por el usuario.
+    """
+    # Expresión regular para validar una fecha en formato dd/mm/yyyy
+    pattern = r"^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/([0-9]{4})$"
+
+    # Pedimos al usuario que ingrese una fecha
+    date = input(message)
+
+    # Validamos la entrada del usuario
+    while not re.match(pattern, date):
+        print("Fecha inválida, ingrese una fecha válida en formato dd/mm/yyyy")
+        date = input(message)
+
+    return date
+
+
+def input_numero(message):
+    """
+    Pide al usuario que ingrese un número y valida la entrada.
+
+    Args:
+    message (str): mensaje a mostrar al usuario al pedir el número.
+
+    Returns:
+    int: número válido ingresado por el usuario.
+    """
+    # Pedimos al usuario que ingrese un número
+    number = input(message)
+
+    # Validamos la entrada del usuario
+    while not number.isdigit():
+        print("Ingreso inválido, ingrese un número válido")
+        number = input(message)
+
+    return int(number)
+
+
+def input_yes_no(message):
+    """
+    Pide al usuario que ingrese una entrada sí o no y valida la entrada.
+
+    Args:
+    message (str): mensaje a mostrar al usuario al pedir la entrada.
+
+    Returns:
+    bool: 'True' o 'False' según la entrada del usuario.
+    """
+    while True:
+        # Pedimos al usuario que ingrese una entrada sí o no
+        response = input(message)
+
+        # Validamos la entrada del usuario
+        if response.lower() == "sí" or response.lower() == "yes":
+            return True
+        elif response.lower() == "no":
+            return False
+        else:
+            print("Por favor ingrese 'sí' o 'no' / 'yes' or 'no'.")
+
+
+def input_password(message):
+    """
+    Solicita una contraseña al usuario y la valida para asegurarse de que cumple con los siguientes requisitos:
+      - Tiene al menos 8 caracteres
+      - Tiene al menos un dígito
+      - Tiene al menos una mayúscula
+      - Tiene al menos una minúscula
+    Mientras el usuario escribe la contraseña, se muestran asteriscos en lugar de los caracteres ingresados.
+    """
+    import getpass
+
+    while True:
+        password = getpass.getpass(message)
+        if len(password) < 8:
+            print("La contraseña debe tener al menos 8 caracteres. Por favor, inténtelo de nuevo.")
+        elif not any(char.isdigit() for char in password):
+            print("La contraseña debe tener al menos un dígito. Por favor, inténtelo de nuevo.")
+        elif not any(char.isupper() for char in password):
+            print("La contraseña debe tener al menos una mayúscula. Por favor, inténtelo de nuevo.")
+        elif not any(char.islower() for char in password):
+            print("La contraseña debe tener al menos una minúscula. Por favor, inténtelo de nuevo.")
+        else:
+            return password
