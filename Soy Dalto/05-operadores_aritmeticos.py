03c770d997ac27183f2d41a8462b5c23c9697e17
Agustín Comolli <agustincomolli@gmail.com>
2023-07-05 11:31:13 -0300

    Agregado ejercicios 5 y 6

---
+++ Soy Dalto/05-operadores_aritmeticos.py
@@ -0,0 +1,27 @@
+# Suma (+)
+add = 12 + 5
+print(f"Suma 12 + 5 = {add}")
+
+# Resta (-)
+substract  = 12 - 5
+print(f"Resta 12 - 5 = {substract}")
+
+# Multiplicación (*)
+multiplication = 12 * 5
+print(f"Multiplica 12 * 5 = {multiplication}")
+
+# División (/) -> Devuelve un dato float
+division = 12 / 5 
+print(f"Divide 12 / 5 = {division}")
+
+# Módulo (%) -> Devuelve el resto de la división
+module = 12 % 5
+print(f"Módulo 12 % 5 = {module}")
+
+# Exponenciación (**)
+potency = 12 ** 5
+print(f"Potencia 12 ** 5 = {potency}")
+
+# División baja (//) -> Devuelve la parte entera de la división
+integer_division = 12 // 5
+print(f"División entera 12 // 5 = {integer_division}")
