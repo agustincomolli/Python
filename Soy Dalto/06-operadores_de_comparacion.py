2960c1c7d4c1e3887156dd4b52d9db4f7f08a554
Agustín Comolli <agustincomolli@gmail.com>
2023-07-06 11:24:12 -0300

    Terminado ejercicio 06

---
+++ Soy Dalto/06-operadores_de_comparacion.py
@@ -0,0 +1,29 @@
+# Igual a (==)
+is_equal = 5 == 6
+print(f"¿5 es igual a 6?: {is_equal}")
+
+# No es igual a (!=)
+is_not_equal = 5 != 6
+print(f"¿5 NO es igual a 6?: {is_not_equal}")
+
+# Mayor que (>)
+is_greater = 5 > 6
+print(f"¿5 es mayor que 6?: {is_greater}")
+
+# Menor que (<)
+is_minor = 5 < 6
+print(f"¿5 es menor que 6?: {is_minor}")
+
+# Mayor o igual a (>=)
+is_greater_or_equal = 5 >= 6
+print(f"¿5 es mayor o igual a 6?: {is_greater_or_equal}")
+
+# Menor o igual a (<=)
+is_minor_or_equal = 5 <= 6
+print(f"¿5 es menor o igual a 6?: {is_minor_or_equal}")
+
+# Comparando cadenas
+correct_user = "username"
+input_user = "my_name"
+is_valid_user = correct_user == input_user
+print(f"\n¿El usuario es válido? -> {is_valid_user}")
